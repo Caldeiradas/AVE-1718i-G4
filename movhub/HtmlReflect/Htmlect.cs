367dfbadc09a7ad54b084207c5db508b3ae445d4
RafaelDomingues41431 <41431@alunos.isel.ipl.pt>
2017-10-05 22:23:14 +0100

    ToHtml(object[] arr) objs do mesmo tipo not tested

--- movhub/HtmlReflect/Htmlect.cs
+++ movhub/HtmlReflect/Htmlect.cs
@@ -12,34 +12,8 @@ namespace HtmlReflect
         public string ToHtml(object obj)
         {
             List<PropertyInfo> res;
-            String str = "";
             PropertyInfo[] fs = obj.GetType().GetProperties();
             res = new List<PropertyInfo>();
-            str = HtmlBuild(fs,obj);
-            //foreach (PropertyInfo p in fs)
-            //{
-
-            //    object[] attrs = p.GetCustomAttributes(typeof(HtmlIgnoreAttribute), true);
-            //    if (attrs.Length != 0) continue;
-
-            //    Console.WriteLine("Property Name= {0}, Value = {1}" , p.Name , p.GetValue(obj) );
-            //    res.Add(p);
-
-            //    //object[] attrs = p.GetCustomAttributes(typeof(LoggableAttribute), true);
-            //    //if (attrs.Length == 0) continue;
-            //    //res.Add(p);
-            //}
-            return str;
-        }
-
-        public string ToHtml(object[] arr)
-        {
-            return "";
-        }
-
-
-        private string HtmlBuild(PropertyInfo[] fs, object obj)
-        {
             StringBuilder sb = new StringBuilder();
             sb.Append("<ul class='list-group'>");
             foreach (PropertyInfo p in fs)
@@ -57,8 +31,51 @@ namespace HtmlReflect
             return sb.ToString();
         }
 
-    }
+        public string ToHtml(object[] arr)
+        {
+            StringBuilder tableHeader = new StringBuilder();
+            StringBuilder tableContent = new StringBuilder();
 
+            PropertyInfo[] allProperties = arr[0].GetType().GetProperties();
+            LinkedList<PropertyInfo> notIgnoredProperties = new LinkedList<PropertyInfo>();
+
+            //table header row
+            foreach (PropertyInfo currProperty in allProperties)
+            {
+                object[] attrs = currProperty.GetCustomAttributes(typeof(HtmlIgnoreAttribute), true);
+                notIgnoredProperties.AddLast(currProperty);
+                if (attrs.Length != 0) continue;
+                if (tableHeader.Length == 0)
+                    tableHeader.Append("<table class ='table table-hover'> <thread> <tr><th>" + currProperty.Name);
+                else
+                    tableHeader.Append("</th><th>" + currProperty.Name);
+            }
+            tableHeader.Append("</th></tr> </thread>");
+
+            int propertyCount = notIgnoredProperties.Count;
+            //table content
+
+            tableContent.Append("<tbody>");
+            foreach (object currObject in arr)
+            {
+
+                int i = propertyCount;
+
+                tableContent.Append("<tr><td>");
+
+                foreach (PropertyInfo currObjectProperty in notIgnoredProperties)
+                {
+                    if (i > 1)
+                        tableContent.Append(currObjectProperty.GetValue(currObject) + "</td><td>");
+                    else
+                        tableContent.Append(currObjectProperty.GetValue(currObject) + "</td><tr>");
+                }
+            }
+            tableContent.Append("</tbody> </table>");
+            return tableHeader.ToString()+tableContent.ToString();
+        }
+
+    }
 
     public class HtmlIgnoreAttribute : Attribute
     {
