4cab852274cc085cdcd766bc3cf49c0805642c50
Caldeirdas <A41465@alunos.isel.pt>
2017-10-05 15:53:10 +0100

    Início da ciração do entidade "Person"

--- movhub/MovHubDb/Model/Person.cs
+++ movhub/MovHubDb/Model/Person.cs
@@ -2,6 +2,21 @@
 {
     public class Person
     {
-     
+        public string birthday { get; set; }
+        public string name { get; set; }
+        public int imdb_id { get; set; }
+    }
+
+    public class Date
+    {
+        private int Year;
+        private int Month;
+        private int Day;
+
+        /*
+        public Data (string val){
+            Year = int.Parse(val.Substring(0).EndsWith("-"))
+        }
+        */
     }
 }
