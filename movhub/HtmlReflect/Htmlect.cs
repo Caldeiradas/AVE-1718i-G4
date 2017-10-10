b750017d6b8679a35e8f53d8ecc30ff75de6257d
PRECIOUS\Fabio Martin <fabiomartin2006@gmail.com>
2017-10-10 20:22:30 +0100

    ToHtmlArrayTest() , ToHtmlTest()

--- movhub/HtmlReflect/Htmlect.cs
+++ movhub/HtmlReflect/Htmlect.cs
@@ -11,9 +11,7 @@ namespace HtmlReflect
     {
         public string ToHtml(object obj)
         {
-            List<PropertyInfo> res;
             PropertyInfo[] fs = obj.GetType().GetProperties();
-            res = new List<PropertyInfo>();
             StringBuilder sb = new StringBuilder();
             sb.Append("<ul class='list-group'>");
             foreach (PropertyInfo p in fs)
@@ -27,11 +25,8 @@ namespace HtmlReflect
                     continue;
                 }
                 Console.WriteLine("Property Name= {0}, Value = {1}", p.Name, p.GetValue(obj));
-                sb.Append("<li class='list-group-item'><strong>");
-                sb.Append(p.Name);
-                sb.Append("</strong>:");
-                sb.Append(p.GetValue(obj));
-                sb.Append("</li>");
+                sb.Append(
+                    String.Format("<li class='list-group-item'><strong>{0}</strong>:{1}</li>", p.Name, p.GetValue(obj)));
             }
             sb.Append("</ul>");
             return sb.ToString();
@@ -39,43 +34,35 @@ namespace HtmlReflect
 
         public string ToHtml(object[] arr)
         {
-
             if (arr==null || arr.Length == 0) return null;
 
             StringBuilder tableHeader = new StringBuilder();
             StringBuilder tableContent = new StringBuilder();
 
-            
             LinkedList<PropertyInfo> notIgnoredProperties = new LinkedList<PropertyInfo>();
 
-
             //table header 
-            tableHeader.Append("<table class ='table table-hover'> <thread> <tr>");
+            tableHeader.Append("<table class ='table table-hover'> <thead> <tr>");
             foreach (PropertyInfo currProperty in arr[0].GetType().GetProperties())
             {
                 object[] attrs = currProperty.GetCustomAttributes(typeof(HtmlIgnoreAttribute), true);
-                
-
                 if (attrs.Length != 0)
                 {
-                    
                     Console.WriteLine(attrs[0].ToString());
+                    Console.WriteLine(currProperty.Name);
                     continue;
                 }
                 notIgnoredProperties.AddLast(currProperty);
-                
-                    tableHeader.Append("<th>" + currProperty.Name+"</th>");
+                tableHeader.Append("<th>" + currProperty.Name+"</th>");
                 
             }
-            tableHeader.Append("</tr> </thread>");
-
+            tableHeader.Append("</tr> </thead>");
 
             //table content
             tableContent.Append("<tbody>");
             //each object is a table row starts at <tr> ends in </tr>
             foreach (object currObject in arr)
             {
-
                 tableContent.Append("<tr>");
 
                 //each property is table data in the current row starts at <td> ends in </td>
@@ -86,17 +73,14 @@ namespace HtmlReflect
                     {
                         tableContent.Append(attribute.htmlRef.Replace("{name}", currObjectProperty.Name).Replace("{value}", currObjectProperty.GetValue(currObject).ToString()));
                         continue;
-                    }
-                     
-                        tableContent.Append("<td>" + currObjectProperty.GetValue(currObject) + "</td>");
+                    }           
+                    tableContent.Append("<td>" + currObjectProperty.GetValue(currObject) + "</td>");
                 }
-
                 tableContent.Append("</tr>");
             }
             tableContent.Append("</tbody> </table>");
             return tableHeader.ToString()+tableContent.ToString();
         }
-
     }
 
 
--- movhub/HtmlReflectTest/HtmlectTest.cs
+++ movhub/HtmlReflectTest/HtmlectTest.cs
@@ -9,25 +9,30 @@ namespace HtmlReflectTest
     [TestClass]
     public class HtmlectTest
     {
-
-
         [TestMethod]
         public void ToHtmlTest()
         {
 
             TheMovieDbClient movieDb = new TheMovieDbClient();
-            Htmlect html = new Htmlect();
             Movie movie = movieDb.MovieDetails(860);
-            html.ToHtml(movie);
+            Htmlect html = new Htmlect();
+            String thisHtml = html.ToHtml(movie);
+            Assert.IsTrue(
+                thisHtml.Contains("<ul class='list-group'><li class='list-group-item'>" +
+                                  "<strong>OriginalTitle</strong>:WarGames</li>"));
 
         }
-
 
         [TestMethod]
         public void ToHtmlArrayTest()
         {
+            TheMovieDbClient movieDb = new TheMovieDbClient();
+            CreditsItem[] credits = movieDb.MovieCredits(860);
+            Htmlect html = new Htmlect();
+            String thisHtml = html.ToHtml(credits);
+            Assert.IsTrue(thisHtml.Contains("<tbody><tr><td><a href='/person/4756/movies'> 4756 </a>" +
+                                            "</td><td>David Lightman</td><td>Matthew Broderick</td></tr><tr><td>"));
 
-            
         }
 
     }
