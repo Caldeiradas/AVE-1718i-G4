fd07d893a99e34ea029154c1ed144a0ced6184f8
Caldeirdas <A41465@alunos.isel.pt>
2017-10-11 11:54:44 +0100

    Criação da class de teste de atributos e do método de teste do JsonAttribut

---
+++ movhub/HtmlReflectTest/AttributesTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MovHubDb;
+using MovHubDb.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtmlReflectTest
+{
+    [TestClass]
+    class AttributesTest
+    {
+        private TheMovieDbClient movieDb = new TheMovieDbClient();
+
+        /*
+         * Se este parametro estiver a NULL é sinal que 
+         * [JsonProperty("vote_average")]não está a funcionar 
+         */
+        [TestMethod]
+        public void JsonAttributTest()
+        {
+            Movie movie = movieDb.MovieDetails(1018);
+            Assert.IsNotNull(movie.VoteAverage); // origainalmente é Vote_Average em Json
+        }
+    }
+}
+    
