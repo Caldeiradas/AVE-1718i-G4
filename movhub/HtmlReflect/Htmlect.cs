cdb96c4baa8b8bb771798aaf28441152b220b298
PRECIOUS\Fabio Martin <fabiomartin2006@gmail.com>
2017-10-11 13:43:32 +0100

    Varios refactors em Htmlect: 1.adicionei dois dicionarios para introduzir a cache: - notIgnoredProperties para guardar as props sem atributo Ignore - htmlAsProperties para guardar as props com atributo HtmlAs 2.Criei metodo GetPropsWithCustomAttrib: - Devolve as propriedades do objecto nao marcadas com Ignore - Guarda na cache(notIgnoredProperties) o mapa entre o tipo e estas props assim se formos chamar este tipo novamente o dicionario devolve as props respectivas evitando-se reflexao - Eliminei o metodo HtmlAsAttributeExists() - Criei o método GetHtmlAsAttribStringRef(), que recebe a prop verifica se esta tem HtmlAsAttrib se tiver guarda na cache(htmlAsProperties) devolve string Refactor HtmlectTest: - Criei dois testes para testar a cache Refactor program: Terei partes que estavam comentadas Comentei o TestMovieDBWebAppi

--- movhub/HtmlReflect/Htmlect.cs
+++ movhub/HtmlReflect/Htmlect.cs
@@ -4,21 +4,33 @@ using System.Reflection;
 using System.Text;
 
 namespace HtmlReflect
-{ 
+{
     public class Htmlect
     {
+        // maps between a Type and its Properties that aren't marked with the custom attribute HtmlIgnore
+        static Dictionary<Type, List<PropertyInfo>> notIgnoredProperties = new Dictionary<Type, List<PropertyInfo>>();
+        // maps between a Type and its Properties that are marked with the custom attribute HtmlAs
+        static Dictionary<PropertyInfo, string> htmlAsProperties = new Dictionary<PropertyInfo, string>();
+
         public string ToHtml(object obj)
         {
+            List<PropertyInfo> props = new List<PropertyInfo>();
+            props = GetPropsWithCustomAttrib(obj.GetType());
+
             StringBuilder sb = new StringBuilder();
             sb.Append("<ul class='list-group'>");
-            PropertyInfo[] fs = obj.GetType().GetProperties();
-            foreach (PropertyInfo p in fs)
+
+            foreach (PropertyInfo p in props)
             {
-                object[] attrs = p.GetCustomAttributes(typeof(HtmlIgnoreAttribute), true);
-                if (attrs.Length != 0) continue;
-                if(HtmlAsAttributeExists(obj, p, sb))continue ;
-                sb.Append(
-                    String.Format("<li class='list-group-item'><strong>{0}</strong>:{1}</li>", p.Name, p.GetValue(obj)));
+                //if (HtmlAsAttributeExists(obj, p, sb)) continue;
+                string htmlRef = GetHtmlAsAttribStringRef(p);
+                if (htmlRef != null)
+                {
+                    sb.Append(htmlRef.Replace("{name}", p.Name).Replace("{value}", p.GetValue(obj).ToString()));
+                    continue;
+                }
+
+                sb.Append(String.Format("<li class='list-group-item'><strong>{0}</strong>:{1}</li>", p.Name, p.GetValue(obj)));
             }
             sb.Append("</ul>");
             return sb.ToString();
@@ -26,21 +38,19 @@ namespace HtmlReflect
 
         public string ToHtml(object[] arr)
         {
-            if (arr==null || arr.Length == 0) return null;
+            if (arr == null || arr.Length == 0) return null;
 
             StringBuilder tableHeader = new StringBuilder();
             StringBuilder tableContent = new StringBuilder();
 
-            LinkedList<PropertyInfo> notIgnoredProperties = new LinkedList<PropertyInfo>();
+            List<PropertyInfo> props = GetPropsWithCustomAttrib(arr[0].GetType());
 
             //table header 
             tableHeader.Append("<table class ='table table-hover'> <thead> <tr>");
-            foreach (PropertyInfo currProperty in arr[0].GetType().GetProperties())
+
+            foreach (PropertyInfo currProperty in props)
             {
-                object[] attrs = currProperty.GetCustomAttributes(typeof(HtmlIgnoreAttribute), true);
-                if (attrs.Length != 0)continue;
-                notIgnoredProperties.AddLast(currProperty);
-                tableHeader.Append("<th>" + currProperty.Name+"</th>");  
+                tableHeader.Append("<th>" + currProperty.Name + "</th>");
             }
             tableHeader.Append("</tr> </thead>");
 
@@ -52,29 +62,77 @@ namespace HtmlReflect
                 tableContent.Append("<tr>");
 
                 //each property is table data in the current row starts at <td> ends in </td>
-                foreach (PropertyInfo currObjectProperty in notIgnoredProperties)
+                foreach (PropertyInfo currObjectProperty in props)
                 {
-                    if (HtmlAsAttributeExists(currObject, currObjectProperty, tableContent)) continue;
+                    string htmlRef = GetHtmlAsAttribStringRef(currObjectProperty);
+                    if (htmlRef != null)
+                    {
+                        tableContent.Append(htmlRef.Replace("{name}", currObjectProperty.Name).Replace("{value}", currObjectProperty.GetValue(currObject).ToString()));
+                        continue;
+                    }
                     tableContent.Append("<td>" + currObjectProperty.GetValue(currObject) + "</td>");
                 }
                 tableContent.Append("</tr>");
             }
             tableContent.Append("</tbody> </table>");
-            return tableHeader.ToString()+tableContent.ToString();
+            return tableHeader.ToString() + tableContent.ToString();
         }
 
-        private bool HtmlAsAttributeExists(Object obj, PropertyInfo p, StringBuilder sb)
+        /// <summary>
+        ///  Receives a type and checks if it exist in cache(dictionary notIgnoredProperties) 
+        ///  if its exists returns the value stored for that property. 
+        ///  If it doesn't exist checks each property for the IgnoreAttribute
+        ///  If the property doesn't have this attribute it's add to cache and the list to return.
+        ///  </summary>
+        /// <param name="klass">
+        /// type in which to check list of properties
+        /// </param>
+        /// <returns>
+        ///  returns a list of properties not marked with HtmlIgnoreAttribute
+        ///  </returns>
+
+        private List<PropertyInfo> GetPropsWithCustomAttrib(Type klass)
         {
+            List<PropertyInfo> res;
+            if (notIgnoredProperties.TryGetValue(klass, out res)) return res;
+            PropertyInfo[] props = klass.GetProperties();
+            res = new List<PropertyInfo>();
+            foreach (PropertyInfo p in props)
+            {
+                object[] attrs = p.GetCustomAttributes(typeof(HtmlIgnoreAttribute), true);
+                if (attrs.Length != 0) continue;
+                res.Add(p);
+            }
+            notIgnoredProperties.Add(klass, res);
+            return res;
+        }
+        
+        /// <summary>
+        /// Checks the cache, htmlsAsProperties dictionary, if the given property exists
+        /// If it existe returns the stored value
+        /// If it the property doesn t exist in cache it checks via reflection 
+        /// if the property has the HtmlAsAttribute
+        /// </summary>
+        /// <param name="p">the property in which there could be the HtmlAsAttribute</param>
+        /// <returns>
+        /// string provided by custom attribute HtmlAsAttribute  
+        /// If theres HtmlAsAtttribute in p returns null
+        /// </returns>
+
+        private string GetHtmlAsAttribStringRef(PropertyInfo p)
+        {
+            String htmlRef = "";
+            if (htmlAsProperties.TryGetValue(p, out htmlRef)) return htmlRef;
+
             HtmlAsAttribute attribute = (HtmlAsAttribute)p.GetCustomAttribute(typeof(HtmlAsAttribute));
             if (attribute != null)
             {
-                sb.Append(attribute.htmlRef.Replace("{name}", p.Name).Replace("{value}", p.GetValue(obj).ToString()));
-                return true;
+                htmlAsProperties.Add(p, attribute.htmlRef);
+                return attribute.htmlRef;
             }
-            return false;
+            return null;
         }
     }
-
 
     public class HtmlIgnoreAttribute : Attribute
     {
--- movhub/HtmlReflectTest/HtmlectTest.cs
+++ movhub/HtmlReflectTest/HtmlectTest.cs
@@ -17,11 +17,27 @@ namespace HtmlReflectTest
         {
             Movie movie = movieDb.MovieDetails(860);
             String thisHtml = html.ToHtml(movie);
+
             Assert.IsTrue(
                 thisHtml.Contains("<ul class='list-group'><li class='list-group-item'>" +
                                   "<strong>OriginalTitle</strong>:WarGames</li>"));
 
         }
+
+        [TestMethod]
+        public void ToHtmlCacheTest()
+        {
+            Movie movie = movieDb.MovieDetails(860);
+            String thisHtml = html.ToHtml(movie);
+            Movie movie2 = movieDb.MovieDetails(860);
+            String thisHtml2 = html.ToHtml(movie);
+
+            Assert.IsTrue(
+                thisHtml2.Contains("<ul class='list-group'><li class='list-group-item'>" +
+                                  "<strong>OriginalTitle</strong>:WarGames</li>"));
+
+        }
+
 
         // Utilizou-se para este teste o id 15008 pois pertence a um actriz ja falecido
         // assim sendo o dados de teste são imutaveis
@@ -30,7 +46,19 @@ namespace HtmlReflectTest
         {
             MovieSearchItem[] personCredits = movieDb.PersonMovies(15008);
             String thisHtml = html.ToHtml(personCredits);
-            Assert.IsTrue(thisHtml.Contains("<tr><td><a href='/person/1018/movies'> 1018 </a></td><td>Mulholland Drive</td>"));
+            Assert.IsTrue(thisHtml.Contains("</td><td>Mulholland Drive</td><td>2001-05-16</td><td>7,7</td></tr>"));
+
+        }
+
+        [TestMethod]
+        public void ToHtmlArrayCacheTest()
+        {
+            MovieSearchItem[] personCredits = movieDb.PersonMovies(15008);
+            String thisHtml = html.ToHtml(personCredits);
+            MovieSearchItem[] personCredits2 = movieDb.PersonMovies(15008);
+            String thisHtml2 = html.ToHtml(personCredits);
+
+            Assert.IsTrue(thisHtml2.Contains("</td><td>Mulholland Drive</td><td>2001-05-16</td><td>7,7</td></tr>"));
 
         }
 
